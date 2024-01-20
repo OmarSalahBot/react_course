89ad70c362e6411a37d175c225c64180911f6d70
OmarSalahBot <omarsalah8001@gmail.com>
2024-01-20 09:16:20 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1> what are u ding i ccant plz stop iam so get </h1>
+        <div> iam so good but icnat typethat fast </div>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </>
+  );
+}
+
+export default App;
