983b40d7959b059f0bc66e1a4c81677d807c3947
OmarSalahBot <omarsalah8001@gmail.com>
2024-01-21 12:53:28 +0200

    V1.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Pages/Home';
+import Html from './Pages/Html'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement:<h1> Empty Page Error 404</h1>,
+  },
+  
+  {
+    path: "/Html",
+    element: <Html />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
