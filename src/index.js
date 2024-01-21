4cb7fcc1df09db617062d4d6afb82518b08a527b
OmarSalahBot <omarsalah8001@gmail.com>
2024-01-21 12:56:30 +0200

    v1.1

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './Pages/Home';
-import Html from './Pages/Html'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home";
+import Html from "./Pages/Html";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    errorElement:<h1> Empty Page Error 404</h1>,
+    errorElement: <h1> Empty Page Error 404</h1>,
   },
-  
+
   {
     path: "/Html",
     element: <Html />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
